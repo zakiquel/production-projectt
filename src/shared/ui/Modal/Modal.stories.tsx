146af687b4f196d87c53cca247a1be22a9b815bf
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2023-10-21 06:19:11 +0700

    add modal, portal, change header

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from './Modal';
+
+export default {
+  title: 'shared/Modal',
+  component: Modal,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isOpen: true,
+  children: 'Lorem ipasdjaoihfo  ajfpdjfipadjfjipLorem ipasdjaoihfo  ajfpdjfipadjfjip',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  isOpen: true,
+  children: 'Lorem ipasdjaoihfo  ajfpdjfipadjfjipLorem ipasdjaoihfo  ajfpdjfipadjfjip',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import React, { useCallback, useState } from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,9 +11,25 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links} />
+      <Button
+        className={cls.links}
+        theme={ButtonTheme.CLEAR_INVERTED}
+        onClick={onToggleModal}
+      >
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        {/* eslint-disable-next-line */}
+        {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+      </Modal>
     </div>
   );
 };
