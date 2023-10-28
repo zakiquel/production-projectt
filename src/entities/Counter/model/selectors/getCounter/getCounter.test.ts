34c0c0a5604b36b3d4462917748fe747c5452425
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2023-10-28 21:18:09 +0700

    add redux store, add first entity

---
+++ src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -0,0 +1,12 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { getCounter } from 'entities/Counter/model/selectors/getCounter/getCounter';
+
+describe('getCounter', () => {
+  test('Return counter value', () => {
+    const state: DeepPartial<StateSchema> = {
+      counter: { value: 10 },
+    };
+    expect(getCounter(state as StateSchema)).toEqual({ value: 10 });
+  });
+});
---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,25 @@
+import { CounterSchema } from '../types/counterSchema';
+import { counterActions, counterReducer } from './counterSlice';
+
+describe('counterSlice.test', () => {
+  test('Should increment value', () => {
+    const state: CounterSchema = { value: 10 };
+    expect(counterReducer(
+      state,
+      counterActions.increment(),
+    )).toEqual({ value: 11 });
+  });
+  test('Should decrement value', () => {
+    const state: CounterSchema = { value: 10 };
+    expect(counterReducer(
+      state,
+      counterActions.decrement(),
+    )).toEqual({ value: 9 });
+  });
+  test('Should work with empty state', () => {
+    expect(counterReducer(
+      undefined,
+      counterActions.decrement(),
+    )).toEqual({ value: -1 });
+  });
+});
