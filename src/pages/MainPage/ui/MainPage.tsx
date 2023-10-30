01f0a7288305e317a8f0b4b860f2a04772d351e6
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2023-10-30 14:28:22 +0700

    add new input, add login, add form ui

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation();
+  const [value, setValue] = useState('');
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
 
   return (
     <div>
