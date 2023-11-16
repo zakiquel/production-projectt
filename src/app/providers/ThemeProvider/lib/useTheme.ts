3a6e96fbe112fdce490ff7031662e0617c4af8f1
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2023-11-16 08:46:33 +0700

    add new theme, add react refresh plugin

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,14 +10,27 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+    switch (theme) {
+    case Theme.DARK:
+      newTheme = Theme.LIGHT;
+      break;
+    case Theme.LIGHT:
+      newTheme = Theme.CUSTOM;
+      break;
+    case Theme.CUSTOM:
+      newTheme = Theme.DARK;
+      break;
+    default:
+      newTheme = Theme.DARK;
+    }
     setTheme?.(newTheme);
     document.body.className = newTheme;
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
   return {
-    theme: theme || Theme.LIGHT,
+    theme: theme || Theme.DARK,
     toggleTheme,
   };
 }
