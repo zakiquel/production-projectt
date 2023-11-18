241d1b11d631e235972bf47e77dc44db01296b89
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2023-11-18 10:46:44 +0700

    update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,6 +2,9 @@ export {
   getUserAuthData,
 } from './model/selectors/getUserAuthData/getUserAuthData';
 export {
+  getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
+export {
   userReducer,
   userActions,
 } from './model/slice/userSlice';
