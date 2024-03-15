7e69dd30ae69a5ee400607f8caee2aca310c3c4f
Кирилл Завалишин <80388663+zakiquel@users.noreply.github.com>
2024-03-15 14:48:19 +0700

    refactoring router

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import React, { useMemo } from 'react';
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 import { getUserAuthData, getUserRoles, UserRole } from '@/entities/User';
+
+import { RoutePath } from '@/shared/const/router';
 
 interface RequireAuthProps {
   children: JSX.Element;
--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -7,7 +7,6 @@ import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
 import { Card } from '@/shared/ui/Card/Card';
 import { Avatar } from '@/shared/ui/Avatar/Avatar';
 import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 import { AppLink } from '@/shared/ui/AppLink/AppLink';
 import { ArticleView, ArticleBlockType } from '../../model/consts/articleConsts';
 import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
@@ -15,6 +14,8 @@ import cls from './ArticleListItem.module.scss';
 import {
   Article, ArticleTextBlock,
 } from '../../model/types/article';
+
+import { RoutePath } from '@/shared/const/router';
 
 interface ArticleListItemProps {
   className?: string;
